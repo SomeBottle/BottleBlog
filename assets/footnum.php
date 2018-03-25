748842b58d50dadf0274fad2eb98f2fa40df6046
SomeBottle <583797659@qq.com>
2018-03-25 22:31:34 +0800

    增强页码上下切换按钮的判断性

--- assets/footnum.php
+++ assets/footnum.php
@@ -1,3 +1,3 @@
 ﻿<?php if(intval($pagec)!==1){?><button type="button" class="btn btn-default" onclick='prepage(<?php echo $pagec;?>)'>上一页</button><?php }?>
             <?php if(empty($pagec)){echo "1";}else{echo $pagec;}?>/<?php echo $totalpage; ?>
-                <?php if($totalpage>1&&intval($pagec)!==$totalpage){?><button type="button" class="btn btn-default" onclick='nextpage(<?php echo $pagec;?>)'>下一页</button><?php }?>
+                <?php if($totalpage>1&&intval($pagec)!==intval($totalpage)){?><button type="button" class="btn btn-default" onclick='nextpage(<?php echo $pagec;?>)'>下一页</button><?php }?>
