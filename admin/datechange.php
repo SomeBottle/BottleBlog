ed88a3cc517537ddfd5c827cd33d4cd8563a0d29
SomeBottle <583797659@qq.com>
2018-04-01 13:50:19 +0800

    修复倒序顺序高效性问题

--- admin/datechange.php
+++ admin/datechange.php
@@ -45,10 +45,50 @@ if ($posttype == "new") { //如果是新文章
 }  else { //如果是编辑文章
 $editnum=str_replace("id","",$status);
 echo "<h1>$editnum</h1>";
+$half=ceil($pnum/2);
     $checknum = $pnum;
 	$stops=0;
 	$findnum=0;
     while ($checknum >= 0&&$stops!==1) {
+        $snum = 0;
+        while ($snum <=($pnum - 1)) {
+            if (file_exists("./../contents/posts/post$snum.php")) {
+                require "./../contents/posts/post$snum.php";
+                $rtitle = $title;
+                $rcontent = $content;
+                $rtag = $tag;
+                $rdate = $date;
+                $rwzid = $wzid;
+                $rpoststring = '<?php $title="' . $rtitle . '";$content="' . $rcontent . '";$date="' . $rdate . '";$tag="' . $rtag . '";$wzid="' . $rwzid . '"; ?>';
+                @include ("./../contents/posts/post" . ($snum + 1) . ".php");
+                $r2title = $title;
+                $r2content = $content;
+                $r2tag = $tag;
+                $r2date = $date;
+                $r2wzid = $wzid;
+                $r2poststring = '<?php $title="' . $r2title . '";$content="' . $r2content . '";$date="' . $r2date . '";$tag="' . $r2tag . '";$wzid="' . $r2wzid . '"; ?>';
+                if ($rdate > $r2date) {
+                    file_put_contents("./../contents/posts/post$snum.php", $r2poststring);
+                    file_put_contents("./../contents/posts/post" . ($snum + 1) . ".php", $rpoststring);
+                }
+            }
+            $snum+= 1;
+        }
+		if(file_exists("./../contents/posts/post$checknum.php")) {
+		require "./../contents/posts/post$checknum.php";
+		$rdate = $date;
+		@include ("./../contents/posts/post" . ($checknum + 1) . ".php");
+		$r2date = $date;
+		if ($rdate < $r2date) {
+                    $stops=1;
+                }
+		}
+        $checknum-= 1;
+    }
+	$checknum = $pnum;
+	$stops=0;
+	$findnum=0;
+	    while ($checknum >= 0&&$stops!==1) {
         $snum = ($pnum - 1);
         while ($snum >=0) {
             if (file_exists("./../contents/posts/post$snum.php")) {
