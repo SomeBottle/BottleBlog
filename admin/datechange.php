9811285b884f955bd91b2485f828075bbb14dfba
SomeBottle <583797659@qq.com>
2018-04-01 09:09:21 +0800

    提高发布新文章的效率

--- admin/datechange.php
+++ admin/datechange.php
@@ -7,9 +7,10 @@ if (!isset($_SESSION['iflogin']) || !isset($_SESSION['username']) || $_SESSION['
  SomeBottle*/
 require "./../contents/posts/postnum.php";
 if ($posttype == "new") { //如果是新文章
-    $realpnum = $pnum - 1;
+    $realpnum = $pnum - 2;
     $rpostdate = $date;
     require "./../contents/posts/post$realpnum.php";
+	echo "<script>alert('$realpnum - $rpostdate - $date');</script>";
     if (intval($rpostdate) < intval($date)) {//如果日期有差别
         $checknum = $pnum;
         while ($checknum >= 0) {
