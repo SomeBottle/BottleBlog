1764f28080da71c780ed88cb41f3ad8e9e87283d
SomeBottle <583797659@qq.com>
2018-04-05 18:15:27 +0800

    修复更新设置缓存不更新的问题

--- admin/postset.php
+++ admin/postset.php
@@ -12,6 +12,7 @@ $blogbeian=$_POST['blogbeian'];
 $stringset='<?php $bname="'.$blogname.'";$bmeta="'.$blogmeta.'";$bhost="'.$bloghost.'";$bavatar="'.$blogavatar.'";$bbeian="'.$blogbeian.'"; ?>';
 file_put_contents("savedconfig/blogconfig.php",$stringset);
 @session_write_close();
+require "cacheclear.php";
 ?>
 <h2>正在保存设置</h2>
 <script>window.open('main.php','_self');</script>
